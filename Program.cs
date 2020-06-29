6d2d891af35c5a2b5cc6040e0ddabb6d8e6efa9a
proyectosnb <66807660+proyectosnb@users.noreply.github.com>
2020-06-29 16:09:30 -0500

    Add files via upload
    Se actualiza proyecto con modificaciones sin terminar de menu principal

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Intramed_gmh
+namespace intramed
 {
     public class Program
     {
